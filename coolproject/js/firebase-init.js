4a0f21881179a92e9cd3a82ab34e936b966fcd08
domainlord0 <Iamamit2official@gmail.com>
2025-07-18 23:53:24 +0530

    Update firebase-init.js

--- coolproject/js/firebase-init.js
+++ coolproject/js/firebase-init.js
@@ -1,10 +1,9 @@
-// Get your config from Firebase Console > Project Settings > SDK setup
 const firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
-  authDomain: "YOUR-PROJECT.firebaseapp.com",
-  projectId: "YOUR-PROJECT-ID",
-  // ...other keys
+  apiKey: "AIzaSyB1ilMMW6LCTO8eGyKwAD5nPbJLdDiu6DA",
+  authDomain: "linkinbio-f70b1.firebaseapp.com",
+  projectId: "linkinbio-f70b1",
+  storageBucket: "linkinbio-f70b1.appspot.com",
+  messagingSenderId: "67493439213",
+  appId: "1:67493439213:web:199390d82cf9b2d99c0ee4",
+  measurementId: "G-5JFLHJECR7"
 };
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
